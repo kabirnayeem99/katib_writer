574ad1ce4fa6aca897bad4f49b4dc099de4c08a6
Naimul Kabir <kabirnayeem.99@gmail.com>
2024-05-05 01:01:22 +0600

    feat: give pwa support

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,22 +1,47 @@
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("v1").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/css/style.css",
-        "/js/script.js",
-        "/icon-192x192.png",
-        "/icon-512x512.png",
-      ]);
-    })
-  );
+const CACHE_NAME = "minimalistic_writer_cache_v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/script.js",
+  "/manifest/icon-192x192.png",
+  "/manifest/icon-256x256.png",
+  "/manifest/icon-384x384.png",
+  "/manifest/icon-212x212.png",
+];
+
+self.addEventListener("install", async (event) => {
+  const cache = await caches.open(CACHE_NAME);
+  await cache.addAll(urlsToCache);
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      // Enable navigation preload if it's supported.
+      // See https://developers.google.com/web/updates/2017/02/navigation-preload
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+    })()
+  );
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", async (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    (async () => {
+      const cachedResponse = await caches.match(event.request);
+      if (cachedResponse) return cachedResponse;
+      else {
+        try {
+          return await fetch(event.request);
+        } catch (error) {
+          console.error("Fetch failed:", error);
+        }
+      }
+    })()
   );
 });
